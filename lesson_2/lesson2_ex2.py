cbb42acdce381885472e0b6aa5ae550335e7b423
DianaChumachenko <dchumachenko0508@gmail.com>
2020-11-14 21:08:47 +0300

    'lesson2'

---
+++ lesson_2/lesson2_ex2.py
@@ -0,0 +1,10 @@
+print('Escape sequences\n',
+      '\\a    Bell (alert)\n',
+      '\\b    Backspace\n',
+      '\\n    New line\n',
+      '\\t    Horizontal tab\n',
+      '\\\\    Backslash \\\n',
+      '\\"    Double quotation mark "\n',
+      '\\\'    Single quotation mark \'\n')
+
+
