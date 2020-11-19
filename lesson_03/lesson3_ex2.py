e1cf6a58d2bda6ef4a54d69348906997ca9248aa
DianaChumachenko <dchumachenko0508@gmail.com>
2020-11-20 00:09:01 +0300

    'lesson3_ex2'

--- lesson_03/lesson3_ex2.py
+++ lesson_03/lesson3_ex2.py
@@ -2,6 +2,6 @@ num1 = int(input('Please enter a number of pupils in the first class: '))
 num2 = int(input('Please enter a number of pupils in the second class: '))
 num3 = int(input('Please enter a number of pupils in the third class: '))
 
-num = (num1+num2+num3) // 2 + (num1+num2+num3) % 2
+num = (num1 // 2 + num2 // 2 + num3 // 2 + num1 % 2 + num2 % 2 + num3 % 2)
 
 print('Result:', num)
