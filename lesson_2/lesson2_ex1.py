f8c82fcf1847f75c385dcbdc73cf9e82c19e29e4
DianaChumachenko <dchumachenko0508@gmail.com>
2020-12-15 21:46:42 +0300

    added lesson2_ex1

--- lesson_2/lesson2_ex1.py
+++ lesson_2/lesson2_ex1.py
@@ -1,1 +1,10 @@
-
+to_print = ("""
+******\t\t***\t\t   *   \t\t*     *\t\t   *\n
+*     *\t\t * \t\t  * *  \t\t**    *\t\t  * *\n
+*     *\t\t * \t\t *   * \t\t* *   *\t\t *   *\n
+*     *\t\t * \t\t*     *\t\t*  *  *\t\t*     *\n
+*     *\t\t * \t\t*******\t\t*   * *\t\t*******\n
+*     *\t\t * \t\t*     *\t\t*    **\t\t*     *\n
+******\t\t***\t\t*     *\t\t*     *\t\t*     *\n
+""")
+print(to_print)
