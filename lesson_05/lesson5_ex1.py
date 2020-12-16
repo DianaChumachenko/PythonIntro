3bc5373de01316c09600e70d93bdb9cb52b2dc78
DianaChumachenko <74212213+DianaChumachenko@users.noreply.github.com>
2020-12-16 14:48:09 +0200

    Update lesson5_ex1.py
    some changes

--- lesson_05/lesson5_ex1.py
+++ lesson_05/lesson5_ex1.py
@@ -13,10 +13,11 @@ while i < h:
     i += 1
 print()
 
+
 i = 0
 while i < h:
     j = 0
-    while j < h * 2:
+    while j < h * 2 - 1:
         if i == h - 1 - j or i == h - 1 or j == h - 1 + i:
             print('* ', end='')
         else:
@@ -26,29 +27,6 @@ while i < h:
     i += 1
 print()
 
-i = 0
-while i < h:
-    j = 0
-    while j < h * 2:
-        if h - 1 - i <= j <= h - 1 + i:
-            print('* ', end='')
-        else:
-            print('  ', end='')
-        j += 1
-    print()
-    i += 1
-i = 0
-while i < h - 1:
-    j = 0
-    while j < h * 2:
-        if i == j - 1 or j == h - 1 - i:
-            print('* ', end='')
-        else:
-            print('  ', end='')
-        j += 1
-    print()
-    i += 1
-print()
 
 i = 0
 while i < h:
@@ -65,7 +43,32 @@ i = 0
 while i < h - 1:
     j = 0
     while j < h * 2:
-        if i == j - 1 or j == h - 1 - i or j == h - 1:
+        if i == j - 1 or j == h - i - 1 + (h - 1) - 1:
+            print('* ', end='')
+        else:
+            print('  ', end='')
+        j += 1
+    print()
+    i += 1
+print()
+
+
+i = 0
+while i < h:
+    j = 0
+    while j < h * 2:
+        if h - 1 - i <= j <= h - 1 + i:
+            print('* ', end='')
+        else:
+            print('  ', end='')
+        j += 1
+    print()
+    i += 1
+i = 0
+while i < h - 1:
+    j = 0
+    while j < h * 2:
+        if i == j - 1 or j == h - i - 1 + (h - 1) - 1 or j == h - 1:
             print('* ', end='')
         else:
             print('  ', end='')
