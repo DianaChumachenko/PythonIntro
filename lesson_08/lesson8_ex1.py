d754050f35f3b0b31815e9ba8caab6ffd746fdb9
DianaChumachenko <74212213+DianaChumachenko@users.noreply.github.com>
2020-12-22 15:16:16 +0200

    Update lesson8_ex1.py

--- lesson_08/lesson8_ex1.py
+++ lesson_08/lesson8_ex1.py
@@ -1,13 +1,17 @@
-ledger = [
+lst = [
     [34587, 'Learning Python, Mark Lutz', 4, 40.95],
     [98762, 'Programming Python, Mark Lutz', 5, 56.80],
     [77226, 'Head First Python, Paul Barry', 3, 32.95],
     [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99]
 ]
-def func(lst):
-    return round(lst[2]*lst[3], 2)
 
+res = list(map(lambda lst: lst[0], lst))
 
-lst = []
-for i in ledger:
-    lst.append(func(ledger))
+res1 = list(map(lambda lst: round((lst[2]*lst[3]), 2), lst))
+
+for i in range(len(res)):
+    if res[i] < 100:
+        res[i] += 10
+
+result = list(zip(res, res1))
+print(result)
